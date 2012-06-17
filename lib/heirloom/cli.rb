18b9f0e1e7f9908aebf6f15eb1899dd4a7965080
Brett Weaver <brett@weav.net>
2012-06-17 12:21:51 -0700

    udpateing formating of cli

--- lib/heirloom/cli.rb
+++ lib/heirloom/cli.rb
@@ -5,15 +5,17 @@ module Heirloom
     def self.start
       @opts = Trollop::options do
         banner <<-EOS
+
 I build and manage artifacts
 
 Usage:
 
 heirloom list
 heirloom versions -n NAME
-heirloom show -n NAME -i VERSION
-heirloom build -n NAME -i VERSION -d DIRECTORY [-p]
-heirloom destroy -n NAME -i VERSION
+heirloom show -n NAME -i ID
+heirloom build -n NAME -i ID -d DIRECTORY [-p]
+heirloom destroy -n NAME -i ID
+
 EOS
         opt :help, "Display Help"
         opt :directory, "Source directory of artifact build.", :type => :string
@@ -28,20 +30,20 @@ EOS
       case cmd
       when 'list'
         puts a.list
-      when 'versions', 'artifacts'
+      when 'versions', 'artifacts', 'ids'
         puts a.versions :name => @opts[:name]
       when 'show'
         puts a.show(:name => @opts[:name],
-                    :id => @opts[:id]).to_yaml
+                    :id   => @opts[:id]).to_yaml
       when 'build'
-        a.build :name => @opts[:name],
-                :id => @opts[:id],
-                :accounts => @opts[:accounts],
+        a.build :name      => @opts[:name],
+                :id        => @opts[:id],
+                :accounts  => @opts[:accounts],
                 :directory => @opts[:directory],
-                :public => @opts[:public]
+                :public    => @opts[:public]
       when 'destroy', 'delete'
         a.destroy :name => @opts[:name],
-                  :id => @opts[:id]
+                  :id   => @opts[:id]
       else
         puts "Unkown command: '#{cmd}'."
       end

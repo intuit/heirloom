75059702b2d8bc0071763b4a2065b5c5ba37101e
Brett Weaver <brett@weav.net>
2012-07-28 16:05:52 -0700

    adding updater & uploader

--- spec/archive/updater_spec.rb
+++ spec/archive/updater_spec.rb
@@ -6,11 +6,21 @@ describe Heirloom do
     @config_mock = double 'config'
     @logger_mock = double 'logger'
     @config_mock.should_receive(:logger).and_return(@logger_mock)
-    @reader = Heirloom::Updater.new :config => @config_mock,
-                                    :name   => 'tim',
-                                    :id     => '123'
+    @updater = Heirloom::Updater.new :config => @config_mock,
+                                     :name   => 'tim',
+                                     :id     => '123'
   end
 
-  it "should test the archive updater methods"
+  it "should test an attribute is updated" do
+    sdb_mock = mock 'sdb mock'
+    @logger_mock.should_receive(:info)
+    Heirloom::AWS::SimpleDB.should_receive(:new).
+                            with(:config => @config_mock).
+                            and_return sdb_mock
+    sdb_mock.should_receive(:put_attributes).
+             with('tim', '123', { 'attr' => 'val' }, { :replace => 'attr' })
+    @updater.update :attribute => 'attr',
+                    :value     => 'val'
+  end
 
 end
--- spec/archive/uploader_spec.rb
+++ spec/archive/uploader_spec.rb
@@ -6,11 +6,35 @@ describe Heirloom do
     @config_mock = double 'config'
     @logger_mock = double 'logger'
     @config_mock.should_receive(:logger).and_return(@logger_mock)
-    @reader = Heirloom::Uploader.new :config => @config_mock,
-                                     :name   => 'tim',
-                                     :id     => '123'
+    @uploader = Heirloom::Uploader.new :config => @config_mock,
+                                       :name   => 'tim',
+                                       :id     => '123'
   end
 
-  it "should test the archive uploader methods"
+  it "should upload a new archive" do
+    s3_mock = mock 's3'
+    Heirloom::Uploader::S3.should_receive(:new).
+                           with(:config => @config_mock,
+                                :logger => @logger_mock,
+                                :region => 'us-west-1').
+                           and_return s3_mock
+    s3_mock.should_receive(:upload_file).
+            with(:bucket          => 'prefix-us-west-1',
+                 :file            => '/tmp/file',
+                 :id              => '123',
+                 :key_folder      => 'tim',
+                 :key_name        => "123.tar.gz",
+                 :name            => 'tim',
+                 :public_readable => true)
+    s3_mock.should_receive(:add_endpoint_attributes).
+            with(:bucket => 'prefix-us-west-1',
+                 :id     => '123',
+                 :name   => 'tim')
+    @logger_mock.should_receive(:info)
+    @uploader.upload :file            => '/tmp/file',
+                     :bucket_prefix   => 'prefix',
+                     :regions         => ['us-west-1'],
+                     :public_readable => true
+  end
 
 end

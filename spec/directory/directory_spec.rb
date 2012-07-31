3534c71ecbf06ebb901d6cde5d3613e23b893f00
Tommy Bishop <bishop.thomas@gmail.com>
2012-07-31 10:34:16 -0700

    refactor of directory spec

--- spec/directory/directory_spec.rb
+++ spec/directory/directory_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
-describe Heirloom do
+describe Heirloom::Directory do
+
+  describe 'build_artifact_from_directory' do
 
     before do
       @config_mock = double 'config'
@@ -9,9 +11,6 @@ describe Heirloom do
       @directory = Heirloom::Directory.new :config  => @config_mock,
                                            :exclude => ['.', '..', 'dont_pack_me'],
                                            :path    => '/target/dir'
-    end
-
-    it "should build an archive from the latest commit in path" do
       file_mock = double 'file'
       output_mock = double 'output mock'
       Dir.stub :tmpdir => '/tmp/dir'
@@ -22,23 +21,19 @@ describe Heirloom do
       Heirloom::Directory.any_instance.should_receive(:`).
                           with("tar czf /tmp/dir/AAAAAAAA.tar.gz pack_me .hidden").
                           and_return output_mock
+    end
+
+    it "should build an archive from the latest commit in path" do
       $?.should_receive(:success?).and_return true
       @directory.build_artifact_from_directory.should be_true
     end
 
-    it "should return false if the shell is not succesful" do
-      file_mock = double 'file'
-      output_mock = double 'output mock'
-      Dir.stub :tmpdir => '/tmp/dir'
-      Kernel.stub :rand => 0
-      Dir.should_receive(:entries).with('/target/dir').
-                                   exactly(2).times.
-                                   and_return(['pack_me', '.hidden', 'dont_pack_me'])
-      Heirloom::Directory.any_instance.should_receive(:`).
-                          with("tar czf /tmp/dir/AAAAAAAA.tar.gz pack_me .hidden").
-                          and_return output_mock
-      $?.should_receive(:success?).and_return false
-      @directory.build_artifact_from_directory.should be_false
+    context 'when unable to create the tar' do
+      before { $?.stub(:success?).and_return(false) }
+
+      it { @directory.build_artifact_from_directory.should be_false }
     end
+
+  end
 
 end

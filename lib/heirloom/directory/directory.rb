9b4240a39d4e7870d4ff6c446b2387c23fa26871
Tommy Bishop <bishop.thomas@gmail.com>
2012-07-31 16:53:10 -0700

    refactor directory

--- lib/heirloom/directory/directory.rb
+++ lib/heirloom/directory/directory.rb
@@ -4,41 +4,41 @@ module Heirloom
 
   class Directory
 
-    attr_accessor :config, :exclude, :local_build, :path, :logger
+    attr_writer :local_build
 
     def initialize(args)
-      self.config = args[:config]
-      self.exclude = args[:exclude]
-      self.path = args[:path]
-      self.logger = config.logger
+      @config = args[:config]
+      @exclude = args[:exclude]
+      @path = args[:path]
+      @logger = @config.logger
     end
 
     def build_artifact_from_directory
       random_text = (0...8).map{65.+(Kernel.rand(25)).chr}.join
 
-      unless local_build
-        self.local_build = File.join(Dir.tmpdir, random_text + ".tar.gz")
+      unless @local_build
+        @local_build = File.join(Dir.tmpdir, random_text + ".tar.gz")
       end
 
-      logger.info "Building Heirloom '#{local_build}' from '#{path}'."
-      logger.info "Excluding #{exclude.to_s}."
-      logger.info "Adding #{files_to_pack.to_s}."
+      @logger.info "Building Heirloom '#{@local_build}' from '#{@path}'."
+      @logger.info "Excluding #{@exclude.to_s}."
+      @logger.info "Adding #{files_to_pack.to_s}."
 
-      build_archive local_build, files_to_pack
+      build_archive
     end
 
     private
 
-    def build_archive(local_build, files_to_pack)
-      command = "tar czf #{local_build} #{files_to_pack.join(' ')}"
-      logger.info "Archiving with: `#{command}`"
+    def build_archive
+      command = "tar czf #{@local_build} #{files_to_pack.join(' ')}"
+      @logger.info "Archiving with: `#{command}`"
       output = `#{command}`
-      logger.debug "Exited with status: '#{$?.exitstatus}' ouput: '#{output}'"
+      @logger.debug "Exited with status: '#{$?.exitstatus}' ouput: '#{output}'"
       $?.success?
     end
-      
+
     def files_to_pack
-      Dir.entries(path) - ['.', '..'] - exclude
+      Dir.entries(@path) - ['.', '..'] - @exclude
     end
 
   end

64cbee4511c3d1b15bab03c2ab283830c6b6de4e
Brett Weaver <brett@weav.net>
2012-11-26 14:31:12 -0800

    fixing stub

--- spec/downloader/s3_spec.rb
+++ spec/downloader/s3_spec.rb
@@ -8,7 +8,7 @@ describe Heirloom do
 
     @config_mock = double 'config'
     @logger_mock = double 'logger'
-    @config_mock.should_receive(:logger).and_return(@logger_mock)
+    @config_mock.stub :logger => @logger_mock
     @s3 = Heirloom::Downloader::S3.new :config  => @config_mock,
                                        :region  => 'us-west-1'
   end

af40a5d4a630bccfd8ef6cfc39be419a726cd87b
keviny22 <kevin_young@intuit.com>
2013-03-26 16:18:53 -0700

    Added s3 url to catalog command output

--- lib/heirloom/cli/formatter/catalog.rb
+++ lib/heirloom/cli/formatter/catalog.rb
@@ -30,9 +30,13 @@ module Heirloom
 
         def details
           data = @catalog.each_pair.map do |k,v|
+            urls = []
+            @catalog[k]["regions"].each {|region|urls.push("s3://" + @catalog[k]["bucket_prefix"].first + "-" + region + "/" + k)}
+
             d = k + "\n"
             d << "  Regions       : " + @catalog[k]["regions"].join(", ") + "\n"
-            d << "  Bucket Prefix : " + @catalog[k]["bucket_prefix"].first
+            d << "  Bucket Prefix : " + @catalog[k]["bucket_prefix"].first + "\n"
+            d << "  URLs          : " + urls.join("\n                  ")
           end
           data.join "\n"
         end

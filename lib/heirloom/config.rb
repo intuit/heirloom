c0a160d8c4db6e9ca5df5090d3c5efb5e668bd14
Dylan Vaughn <dylancvaughn@gmail.com>
2013-04-09 10:58:03 -0700

    moving from indirect to direct variable access

--- lib/heirloom/config.rb
+++ lib/heirloom/config.rb
@@ -4,20 +4,20 @@ module Heirloom
     attr_accessor :access_key, :secret_key, :metadata_region, :logger, :environment
 
     def initialize(args={})
-      @opts            = args[:opts] ||= Hash.new
-      self.logger      = args[:logger] ||= HeirloomLogger.new
-      self.environment = args[:environment] ||= 'aws'
-      @config          = load_config_file
+      @opts        = args[:opts] ||= Hash.new
+      @logger      = args[:logger] ||= HeirloomLogger.new
+      @environment = args[:environment] ||= 'aws'
+      @config      = load_config_file
       load_config
     end
 
     def load_config
-      self.access_key      = @opts.fetch :aws_access_key, 
-                                         @config['access_key']
-      self.secret_key      = @opts.fetch :aws_secret_key, 
-                                         @config['secret_key']
-      self.metadata_region = @opts.fetch :metadata_region, 
-                                         @config['metadata_region']
+      @access_key      = @opts.fetch :aws_access_key, 
+                                     @config['access_key']
+      @secret_key      = @opts.fetch :aws_secret_key, 
+                                     @config['secret_key']
+      @metadata_region = @opts.fetch :metadata_region, 
+                                     @config['metadata_region']
     end
 
     private
@@ -27,10 +27,10 @@ module Heirloom
 
       if File.exists? config_file
         data = YAML::load File.open(config_file)
-        if data.has_key? self.environment
-          data[self.environment]
+        if data.has_key? @environment
+          data[@environment]
         else
-          self.logger.error "Environment '#{self.environment}' not found in config file."
+          @logger.error "Environment '#{@environment}' not found in config file."
           exit 1
         end
       else

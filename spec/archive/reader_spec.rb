b511a1e6cdf1b3068aaec810c2f0c3730708facb
Brett Weaver <brett@weav.net>
2013-04-09 10:00:02 -0700

    updated for the eye

--- spec/archive/reader_spec.rb
+++ spec/archive/reader_spec.rb
@@ -6,8 +6,7 @@ describe Heirloom do
     @sdb_mock = mock 'sdb'
     @config_mock = mock 'config'
     @logger_stub = stub :debug => true
-    @config_mock.should_receive(:logger).and_return @logger_stub
-    Heirloom::AWS::SimpleDB.should_receive(:new).and_return @sdb_mock
+    @config_mock.stub :logger => @logger_stub
     @reader = Heirloom::Reader.new :config => @config_mock,
                                    :name   => 'tim',
                                    :id     => '123'
@@ -15,6 +14,7 @@ describe Heirloom do
 
   context "domain does exist" do
     before do
+      Heirloom::AWS::SimpleDB.should_receive(:new).and_return @sdb_mock
       @sdb_mock.stub :domain_exists? => true
     end
 
@@ -23,36 +23,6 @@ describe Heirloom do
                with("select * from `heirloom_tim` where itemName() = '123'").
                and_return( { '123' => { 'value' => [ 'details' ] } } )
       @reader.show.should == { 'value' => 'details' }
-    end
-
-    it "should get object_acls" do
-      @config_mock.stub(:access_key).and_return('the-key')
-      @config_mock.stub(:secret_key).and_return('the-secret')
-      @reader.stub(:regions).and_return(['us-west-1', 'us-west-2'])
-      @reader.stub(:key_name).and_return('mockvalue')
-      @reader.stub(:get_bucket).and_return('mockvalue')
-      @sdb_mock.stub(:select).and_return('value')
-      s3_mock = mock 's3'
-      Heirloom::AWS::S3.should_receive(:new).
-                       with(:config => @config_mock,
-                            :region => 'us-west-1').
-                       and_return s3_mock
-      Heirloom::AWS::S3.should_receive(:new).
-                       with(:config => @config_mock,
-                            :region => 'us-west-2').
-                       and_return s3_mock
-      
-      s3_mock.stub(:get_object_acl).and_return(
-        {"Owner"=>{"ID"=>"123", "DisplayName"=>"lc"}, 
-          "AccessControlList"=>[
-            {"Grantee"=>{"ID"=>"321", "DisplayName"=>"rickybobby"}, "Permission"=>"READ"}, 
-            {"Grantee"=>{"ID"=>"123", "DisplayName"=>"lc"}, "Permission"=>"FULL_CONTROL"}]
-        })
-
-      @reader.show.should == {} 
-      @reader.object_acls.should == { 'us-west-1-permissions' => 'rickybobby:read, lc:full_control', 
-                                      'us-west-2-permissions' => 'rickybobby:read, lc:full_control'
-                                    } 
     end
 
     it "should return an empty hash if item does not exist" do
@@ -80,9 +50,9 @@ describe Heirloom do
       @sdb_mock.should_receive(:select).
                 exactly(3).times.
                 with("select * from `heirloom_tim` where itemName() = '123'").
-                and_return( { '123' => 
-                              { 'us-west-1-s3-url' => 
-                                [ 's3://the-bucket/the-name/123.tar.gz' ]  
+                and_return( { '123' =>
+                              { 'us-west-1-s3-url' =>
+                                [ 's3://the-bucket/the-name/123.tar.gz' ]
                               }
                             } )
       @reader.get_bucket(:region => 'us-west-1').should == 'the-bucket'
@@ -108,9 +78,9 @@ describe Heirloom do
       @sdb_mock.should_receive(:select).
                 exactly(6).times.
                 with("select * from `heirloom_tim` where itemName() = '123'").
-                and_return( { '123' => 
-                              { 'us-west-1-s3-url' => 
-                                ['s3://the-url/the-bucket/123.tar.gz'] 
+                and_return( { '123' =>
+                              { 'us-west-1-s3-url' =>
+                                ['s3://the-url/the-bucket/123.tar.gz']
                               }
                             } )
       @reader.get_key(:region => 'us-west-1').should == 'the-bucket/123.tar.gz'
@@ -134,13 +104,13 @@ describe Heirloom do
       @sdb_mock.should_receive(:select).
                 exactly(1).times.
                 with("select * from `heirloom_tim` where itemName() = '123'").
-                and_return( { '123' => 
-                              { 'us-west-1-s3-url' => 
+                and_return( { '123' =>
+                              { 'us-west-1-s3-url' =>
                                 ['s3://the-url-us-west-1/the-bucket/123.tar.gz'],
-                                'build_by' => 
-                                ['user'], 
-                                'us-east-1-s3-url' => 
-                                ['s3://the-url-us-east-1/the-bucket/123.tar.gz'] 
+                                'build_by' =>
+                                ['user'],
+                                'us-east-1-s3-url' =>
+                                ['s3://the-url-us-east-1/the-bucket/123.tar.gz']
                               }
                             } )
       @reader.regions.should == ['us-west-1', 'us-east-1']
@@ -150,11 +120,41 @@ describe Heirloom do
 
   context "domain does not exist" do
     before do
+      Heirloom::AWS::SimpleDB.should_receive(:new).and_return @sdb_mock
       @sdb_mock.stub :domain_exists? => false
     end
 
     it "should return false if the simpledb domain does not exist" do
       @reader.exists?.should == false
+    end
+  end
+
+  context "object_acl verify" do
+    it "should get object_acls" do
+      regions = ['us-west-1', 'us-west-2']
+      @config_mock.stub :access_key => 'the-key',
+                        :secret_key => 'the-secret'
+      @reader.stub :regions    => regions,
+                   :key_name   => 'mockvalue',
+                   :get_bucket => 'mockvalue'
+      data = { "Owner" => { "ID" => "123", "DisplayName" => "lc" },
+               "AccessControlList" => [
+                { "Grantee" => { "ID" => "321", "DisplayName" => "rickybobby" },
+                                 "Permission" => "READ" },
+                { "Grantee" => { "ID" => "123", "DisplayName" => "lc" },
+                                 "Permission" => "FULL_CONTROL" }]
+             }
+      s3_stub = stub 's3', :get_object_acl => data
+
+      regions.each do |region|
+        Heirloom::AWS::S3.should_receive(:new).
+                          with(:config => @config_mock,
+                               :region => region).
+                          and_return s3_stub
+      end
+
+      @reader.object_acls.should == { 'us-west-1-permissions' => 'rickybobby:read, lc:full_control',
+                                      'us-west-2-permissions' => 'rickybobby:read, lc:full_control' }
     end
   end
 

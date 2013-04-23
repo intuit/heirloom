0c90e47c3d6eb2db6d18886abf5ee4d45fc4d400
Dylan Vaughn <dylancvaughn@gmail.com>
2013-04-23 09:07:24 -0700

    wip commander / logging

--- lib/heirloom/logger.rb
+++ lib/heirloom/logger.rb
@@ -1,17 +1,40 @@
 require 'logger'
 
 module Heirloom
+  module Logging
+    
+    def logger
+      Logging.logger
+    end
+
+    def self.logger(args = {})
+      log_level = args[:log_level] ||= 'info'
+      @logger ||= Logger.new(STDOUT).tap do |l|
+        l.datetime_format = '%Y-%m-%dT%H:%M:%S%z'
+        l.formatter = proc do |severity, datetime, progname, msg|
+          "#{datetime} #{severity} : #{msg}\n"
+        end
+        l.level = log_level.upcase
+      end
+    end
+  end
+
   class HeirloomLogger
     
     require 'forwardable'
 
     extend Forwardable
+    include Logging
 
     def_delegators :@logger, :debug, :error, :info, :warn
 
     def initialize(args = {})
       @log_level = args[:log_level] ||= 'info'
       @logger    = args[:logger] ||= new_logger(args)
+    end
+
+    def self.logger(args = {})
+      @logger ||= Logging.logger(args)
     end
 
     private
@@ -31,4 +54,6 @@ module Heirloom
     end
 
   end
+
+
 end
